89f41928e4a151341a03dd3e130876df4b0c75e7
Crowdbotics <team@crowdbotics.com>
2021-12-16 22:10:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dirapoly_32458/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dirapoly_32458;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dirapoly_32458";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dirapoly_32458",
+  url: "https://dirapoly_32458.botics.co",
+  api: "https://dirapoly_32458.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
